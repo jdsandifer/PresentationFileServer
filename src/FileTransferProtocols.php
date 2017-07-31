eee02c92147cffb074b3cca4f9854b994b650df8
J.D. Sandifer <jdsandifer@users.noreply.github.com>
2017-07-31 15:55:21 -0700

    Create FileTransferProtocols.php

---
+++ src/FileTransferProtocols.php
@@ -0,0 +1,36 @@
+<?php
+
+// namespace Srs;    // caused errors and may not be necessary so commented out for now
+
+require_once __DIR__.'/NoMagicFunctions.php';
+require_once __DIR__.'/FileTransferProtocol.php';
+
+/**
+ * This is a factory for file transfer protocol commands.
+ * 
+ * This allows protocols to be created with out the calling objects knowing
+ * about the specifics of which protocol is for which computer type.
+ */
+class FileTransferProtocols extends NoMagicFunctions
+{
+    /**
+     * Returns the correct protocol object based on the type of computer.
+     */
+    public static function createFor( $computerType ) {
+        // Create the best protocol for the specific type of computer
+        switch ( $computerType ) {
+            case "MAC":
+                $newFileTransferProtocol = new SecondProtocol();    // (object name changed)
+                break;
+            case "PC":
+                $newFileTransferProtocol = new FirstProtocol();    // (object name changed)
+                break;
+            default:
+                throw new Exception( "Can't create file transfer protocol: unknown computer type" );
+                break;
+        }
+        
+        // Return the protocol (only gets here if protocol creation succeeded above)
+        return $newFileTransferProtocol;
+    }
+}
