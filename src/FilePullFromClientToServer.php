72cd87644ed43f9435cb2da9ae967c4952e3da31
J.D. Sandifer <jdsandifer@users.noreply.github.com>
2017-07-31 15:50:41 -0700

    Create FilePullFromClientToServer.php

---
+++ src/FilePullFromClientToServer.php
@@ -0,0 +1,66 @@
+<?php
+
+// namespace Srs;    // caused errors and may not be necessary so commented out for now
+
+require_once __DIR__.'/../autoLoader.php';
+
+class FilePullFromClientToServer extends NoMagicFunctions
+{
+    private $file = null;
+    private $client = null;
+    private $server = null;
+    private $adminName = null;
+    private $transferProtocol = null;
+    
+    /**
+     * @param file File object that represents the file to transfer.
+     * @param client Client object that represents the computer to pull from.
+     * @param server (optional) Server object that represents the host server.
+     * @param adminName (optional) Name of admin user if this was done by admin.
+     */
+    public function __construct( $file, $client, $server = null, $adminName = null ) {
+        $this->file = $file;
+        $this->client = $client;
+        
+        if ( $server !== null ) {
+            $this->server = $server;
+        } else {
+            // Assume AV_SERVER_1 if not provided.
+            $this->server = new Server( "AV_SERVER_1", "10.0.0.100", "/var/www/html/Files/" );
+        }
+        
+        if ( $adminName !== null ) {
+            $this->adminName = $adminName;
+        }
+        
+        $clientComputerType = $this->client->type();
+        
+        // Use the factory function to figure out which file transfer protocol we need
+        // and return the correct type of object
+        $this->transferProtocol = FileTransferProtocols::createFor( $clientComputerType );
+    }
+    
+    /**
+     * Runs the actual file transfer.
+     */
+    public function run() {
+        // Run file transfer and record result.
+        $transferResult = $this->transferProtocol->pullFile( $this->file, $this->client, $this->server );
+        
+        // Log results
+        if ( $transferResult === "SUCCESS" ) {
+            // log file transfer
+            $logMessage = "Pulled {$this->file->name()} from {$this->client->room()} {$this->client->type()} via {$this->transferProtocol}.";
+            $adminPulled = ( $this->adminName !== null );
+            if ( $adminPulled ) {
+                Log::fileTransfer( $logMessage, null, $this->adminName );
+            } else {
+                Log::fileTransfer( $logMessage, $this->file->presenterName() );
+            }
+        } else {
+            // log error
+            $errorMessage = "File pull error: {$transferResult}";
+            Log::error( $errorMessage );
+        }
+    }
+}
